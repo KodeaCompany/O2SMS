ae9c40ab14ba972d6d6eefd1de36cb64075e59ed
clarai <cisotomayor@cirebits.es>
2016-12-09 01:00:36 +0100

    Trial of the interface.

--- Iteration3/src/main/java/presentation/General_Interface.java
+++ Iteration3/src/main/java/presentation/General_Interface.java
@@ -1,22 +1,43 @@
 package presentation;
 
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
 public class General_Interface {
 
-	public void searchDriverSanctions() {
-		
-		
-		
-		throw new UnsupportedOperationException();
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					General_Interface window = new General_Interface();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
-	public void showSanctionInfo() {
-		// TODO - implement General_Interface.showSanctionInfo
-		throw new UnsupportedOperationException();
+	/**
+	 * Create the application.
+	 */
+	public General_Interface() {
+		initialize();
 	}
 
-	public void paySanction() {
-		// TODO - implement General_Interface.paySanction
-		throw new UnsupportedOperationException();
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
